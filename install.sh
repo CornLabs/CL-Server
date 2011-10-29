d7a4a2f53ac28bc4f05f51e3bc640bb01d5d6330
Sabin Marcu <sabinmarcu@gmail.com>
2011-10-29 15:02:46 +0000

    Bugged the debug messages :) All done!
    Signed-off-by: Sabin Marcu <sabinmarcu@gmail.com>

--- install.sh
+++ install.sh
@@ -5,10 +5,8 @@ command -v "npm" >/dev/null && echo "" || i=1
 command -v "git" >/dev/null && echo "" || { echo "This application requires either GIT or NPM to be installed! "; exit 1; }
 
 if [ "$i" -eq 0 ]; then
-	echo "Node"
 	npm install node-static
 else 
-	echo "git"
 	git submodule update --init --recursive
 fi
 script="$(cd -P "$(dirname "$(readlink "$0")")" && pwd)/start.sh"
